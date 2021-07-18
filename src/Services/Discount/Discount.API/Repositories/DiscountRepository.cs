0c51ed3b65d9e1e9d3d0adb90ad6f0c4e42e8f9a
Wolodymyr <76656624+Waldemier@users.noreply.github.com>
2021-07-18 20:11:20 +0300

    Set up docker and docker-compose files, implemented migrating extension method in program.cs

--- src/Services/Discount/Discount.API/Repositories/DiscountRepository.cs
+++ src/Services/Discount/Discount.API/Repositories/DiscountRepository.cs
@@ -22,7 +22,7 @@ namespace Discount.API.Repositories
                 this._configuration.GetValue<string>("DatabaseSettings:ConnectionString"));
 
             // using query like this by Dapper
-            var coupon = await connection.QueryFirstOrDefaultAsync(
+            var coupon = await connection.QueryFirstOrDefaultAsync<Coupon>(
                 "SELECT * FROM Coupon WHERE ProductName = @ProductName", 
                 new { ProductName = productName });
             
@@ -35,7 +35,7 @@ namespace Discount.API.Repositories
         public async Task<bool> CreateDiscountAsync(Coupon coupon)
         {
             using var connection = new NpgsqlConnection(
-                this._configuration.GetValue<string>("DataSettings:ConnectionString"));
+                this._configuration.GetValue<string>("DatabaseSettings:ConnectionString"));
 
             var affected = await connection.ExecuteAsync(
                 @"INSERT INTO Coupon (ProductName, Description, Amount) 
@@ -56,7 +56,7 @@ namespace Discount.API.Repositories
         public async Task<bool> UpdateDiscountAsync(Coupon coupon)
         {
             using var connection = new NpgsqlConnection(
-                this._configuration.GetValue<string>("DataSettings:ConnectionString"));
+                this._configuration.GetValue<string>("DatabaseSettings:ConnectionString"));
 
             var affected = await connection.ExecuteAsync(
                 @"UPDATE Coupon SET ProductName=@ProductName, Description=@Description, Amount=@Amount WHERE Id=@Id",
@@ -77,7 +77,7 @@ namespace Discount.API.Repositories
         public async Task<bool> DeleteDiscountAsync(string productName)
         {
             using var connection = new NpgsqlConnection(
-                this._configuration.GetValue<string>("DataSettings:ConnectionString"));
+                this._configuration.GetValue<string>("DatabaseSettings:ConnectionString"));
 
             var affected = await connection.ExecuteAsync(
                 "DELETE FROM Coupon WHERE ProductName=@ProductName",
